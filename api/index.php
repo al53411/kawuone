af5050d18dbac723f918d2acd2afdabf6545c08c
ALIF SAIROJI <alsyairoji@gmail.com>
2026-07-28 13:55:25 +0700

    Fix autoload path in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,13 +1,39 @@
 <?php
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// 1. Perbaiki Path Autoload & Bootstrap (hapus 'adKawuone/')
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
+// 2. Arahkan Storage ke /tmp
+$app->useStoragePath('/tmp/storage');
+
+$directories = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+];
+
+foreach ($directories as $directory) {
+    if (!is_dir($directory)) {
+        mkdir($directory, 0755, true);
+    }
+}
+
+// 3. Jalankan Kernel Laravel
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+    $request = Request::capture()
 );
 
 $response->send();
