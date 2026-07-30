77ccdeca2095c51b9092321bb41f4632936677b9
ALIF SAIROJI <alsyairoji@gmail.com>
2026-07-30 21:04:21 +0700

    fix: update vercel config and entrypoint

--- api/index.php
+++ api/index.php
@@ -1,19 +1,34 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Menyiapkan folder penyimpanan sementara (/tmp) agar Vercel tidak Error 500
+// 1. Arahkan direktori penyimpanan sementara ke /tmp Vercel
 $storagePath = '/tmp/storage';
+$bootstrapCachePath = '/tmp/bootstrap/cache';
+
+// Buat direktori jika belum ada
 if (!file_exists($storagePath)) {
     mkdir($storagePath . '/framework/views', 0755, true);
     mkdir($storagePath . '/framework/cache/data', 0755, true);
     mkdir($storagePath . '/framework/sessions', 0755, true);
     mkdir($storagePath . '/logs', 0755, true);
 }
-$app->useStoragePath($storagePath);
 
+if (!file_exists($bootstrapCachePath)) {
+    mkdir($bootstrapCachePath, 0755, true);
+}
+
+// 2. Set environment variable untuk bootstrap cache path
+putenv("APP_STORAGE={$storagePath}");
+$_ENV['APP_STORAGE'] = $storagePath;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Bind path storage & bootstrap cache ke /tmp
+$app->useStoragePath($storagePath);
+$app->useBootstrapPath('/tmp/bootstrap');
+
+// 3. Jalankan HTTP Kernel Laravel
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
