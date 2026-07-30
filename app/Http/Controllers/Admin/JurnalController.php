94c14696a4c3fd70ae49cfe47cb1adec749d6ead
ALIF SAIROJI <alsyairoji@gmail.com>
2026-07-30 20:25:57 +0700

    fix vercel runtime version and controller namespaces

--- app/Http/Controllers/Admin/JurnalController.php
+++ app/Http/Controllers/Admin/JurnalController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Guru; // <-- Ubah Admin jadi Guru
+namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\JurnalGuru;
--- app/Http/Controllers/Admin/SekolahController.php
+++ app/Http/Controllers/Admin/SekolahController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Superadmin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Sekolah;
