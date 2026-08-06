20c61ab4de2981463a0371bf9fb0b046c164b6b6
ALIF SAIROJI <alsyairoji@gmail.com>
2026-08-06 09:43:36 +0700

    feat: add favicon to login  layout files

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,7 +11,6 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
     <!-- Scripts -->
