11413a9a83bd8a4cab7e2e0f171b622c68a871d4
ALIF SAIROJI <alsyairoji@gmail.com>
2026-08-06 09:51:38 +0700

    fix: use favicon.ico for native browser support

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Sistem</title>
-    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=4">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}?v=4">
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
     .glass-card {
