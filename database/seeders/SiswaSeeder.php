db7b2e337f16a25d10087917d19c5ab6dac1f931
ALIF SAIROJI <alsyairoji@gmail.com>
2026-08-17 03:39:16 +0700

    Fix SiswaSeeder column name back to nama_siswa

--- database/seeders/SiswaSeeder.php
+++ database/seeders/SiswaSeeder.php
@@ -9,22 +9,12 @@ class SiswaSeeder extends Seeder
 {
     public function run(): void
     {
-        // UBAH DARI INI:
-    Siswa::create([
-        'nisn' => '0123456789',
-        'nama_siswa' => 'Randi Perkasa', // <-- Hapus nama_siswa
-        'kelas_id' => 1,
-        'jenis_kelamin' => 'L',
-        'alamat' => 'Kec. Padas, Ngawi',
-    ]);
-
-    // MENJADI INI:
-    Siswa::create([
-        'nisn' => '0123456789',
-        'nama_lengkap' => 'Randi Perkasa', // <-- Ubah jadi nama_lengkap
-        'kelas_id' => 1,
-        'jenis_kelamin' => 'L',
-        'alamat' => 'Kec. Padas, Ngawi',
-    ]);
+        Siswa::create([
+            'nisn' => '0123456789',
+            'nama_siswa' => 'Randi Perkasa', // ✅ GUNAKAN nama_siswa
+            'kelas_id' => 1,
+            'jenis_kelamin' => 'L',
+            'alamat' => 'Kec. Padas, Ngawi',
+        ]);
     }
 }
