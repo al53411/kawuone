718624c10b5b70ba797f427a49be4aeb1837b8d9
ALIF SAIROJI <alsyairoji@gmail.com>
2026-08-23 10:31:17 +0700

    Fix API route and AuthController

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-// TAMBAHKAN IMPORT CONTROLLER DI SINI:
 use App\Http\Controllers\Api\AuthController; 
 
-Route::post('/login', [AuthController::class, 'login']);
+// Ubah path dari '/login' menjadi '/v1/login'
+Route::post('/v1/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
