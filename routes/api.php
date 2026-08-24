e0604bfc53b418b268765265ba8de04d994ff006
ALIF SAIROJI <alsyairoji@gmail.com>
2026-08-24 11:54:15 +0700

    Set production POST route for api/v1/login

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
-// Route Test untuk memastikan API berjalan
-Route::get('/v1/login', function () {
-    return response()->json(['status' => 'success', 'message' => 'API Vercel Berhasil Aktif!']);
-});
-
 // Route Login POST untuk Android
 Route::post('/v1/login', [AuthController::class, 'login']);
